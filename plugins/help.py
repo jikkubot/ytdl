e5cd84258a45ed8bebb673d36d868446a1f69198
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-01-07 08:08:07 +0100

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,14 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
-    await message.reply_text(helptxt)
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Support Channel 📢", url="https://t.me/FlixBots")],
+        [InlineKeyboardButton(
+            "Bugs & Reports Bot 🤖", url="https://t.me/FlixHelpBot")]
+    ])
+    helptxt = f"Currently Only Supports Youtube Single  (No Playlist) Just Send Any Valid Youtube Url"
+    await message.reply_text(helptxt, reply_markup=joinButton)
+    raise StopPropagation
