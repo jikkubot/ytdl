3ce28d56e0eb1b29a71de8cb0a3043f42ab72fa0
Jikku <66976170+jikkubot@users.noreply.github.com>
2021-04-18 08:45:08 +0400

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Support Channel 📢", url="https://t.me/FlixBots")],
+        [InlineKeyboardButton("Support Channel 📢", url="https://t.me/FilmClubChannel")],
         [InlineKeyboardButton(
-            "Bugs & Reports Bot 🤖", url="https://t.me/FlixHelpBot")]
+            "Bugs & Reports Bot 🤖", url="https://t.me/FilmClubGroup")]
     ])
-    welcomed = f"Hello <b>{message.from_user.first_name},\n\nWelcome To 𝗙𝗟𝗜𝗫 𝗧𝗨𝗕𝗘 𝗕𝗢𝗧 🥳.\n\nI'm A Powered Youtube Download Bot Which Supports Video, Documents & Audio Of All Qualities 😇</b>\n\n<b>Click</b> /help <b>For More Info On How To Use Me</b>"
+    welcomed = f"Hello <b>{message.from_user.first_name},\n\nWelcome To 𝗙C YOU𝗧𝗨𝗕𝗘 𝗕𝗢𝗧 🥳.\n\nI'm A Powered Youtube Download Bot Which Supports Video, Documents & Audio Of All Qualities 😇</b>\n\n<b>Click</b> /help <b>For More Info On How To Use Me</b>"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
